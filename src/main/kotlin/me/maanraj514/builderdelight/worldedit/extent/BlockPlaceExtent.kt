1df9739dbadf7ed3d91cbbd2d17d0f190bba93fc
YesNoBruhbruh <manrajmaan14@yahoo.com>
2024-03-13 18:43:39 +0800

    I set the runnable to 1L making it seem instant.

--- src/main/kotlin/me/maanraj514/builderdelight/worldedit/extent/BlockPlaceExtent.kt
+++ src/main/kotlin/me/maanraj514/builderdelight/worldedit/extent/BlockPlaceExtent.kt
@@ -3,6 +3,7 @@ package me.maanraj514.builderdelight.worldedit.extent
 import com.jeff_media.customblockdata.CustomBlockData
 import com.sk89q.worldedit.WorldEdit
 import com.sk89q.worldedit.bukkit.BukkitAdapter
+import com.sk89q.worldedit.bukkit.BukkitBlockRegistry
 import com.sk89q.worldedit.entity.Player
 import com.sk89q.worldedit.event.extent.EditSessionEvent
 import com.sk89q.worldedit.extent.AbstractDelegateExtent
@@ -10,6 +11,7 @@ import com.sk89q.worldedit.extent.Extent
 import com.sk89q.worldedit.math.BlockVector3
 import com.sk89q.worldedit.world.block.BlockStateHolder
 import me.maanraj514.builderdelight.BuilderDelight
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.block.Block
 import org.bukkit.persistence.PersistentDataType
@@ -27,57 +29,64 @@ class BlockPlaceExtent(
         if (event.actor !is Player) {
             return super.setBlock(location, block)
         }
+        val success = super.setBlock(location, block) // we set the block.
 
-        val task = object : BukkitRunnable() {
-            override fun run() {
-                val wePlayer = event.actor as Player
-                val world = BukkitAdapter.asBukkitWorld(weWorld).world
-                val bukkitBlock: Block = world.getBlockAt(location.blockX, location.blockY, location.blockZ)
+        val wePlayer = event.actor as Player
+        val world = BukkitAdapter.asBukkitWorld(weWorld).world
+        // sometimes this is too early,
+        // so if we set the block to air, it still thinks its blue_wool.
+        // so we make a runnable.
 
-                //TODO: make it so that, people who aren't builders, when they set
-                // the blocks to air, only all the blocks that are non-builder blocks will stay safe.
+        Bukkit.getScheduler().runTaskLater(plugin, Runnable {
+            val bukkitBlock = world.getBlockAt(location.x, location.y, location.z)
 
-                if (plugin.builders.contains(wePlayer.uniqueId)) {
-                    if (bukkitBlock.type == Material.AIR) {
-                        //remove pdc.
-                        reloadPdcOfBlock(bukkitBlock)
-                    } else {
-                        // if the block type is not air, then set the pdc of that block.
-                        setPdcOfBlock(bukkitBlock)
-                    }
-                } else {
-                    if (bukkitBlock.type == Material.AIR) {
-                        println("block material is air!")
-                        // we want to make sure builder blocks STAY.
-                        if (doesBlockHavePdc(bukkitBlock)) {
-                            println("block has pdc!")
-                            val pdc = CustomBlockData(bukkitBlock, plugin)
-                            val material = Material.getMaterial(pdc.get(plugin.BUILDER_BLOCK_KEY, PersistentDataType.STRING) ?: "") ?: Material.AIR
-                            bukkitBlock.type = material // set it back to the way it was before.
-                        }
-                    } else {
-                        println("block material is not air!")
-                        reloadPdcOfBlock(bukkitBlock)
-                    }
-                }
-            }
-        }
+            handleSetBlock(wePlayer, bukkitBlock)
+        }, 1L)
 
-        task.runTaskLater(plugin, 20L)
-
-        return super.setBlock(location, block)
+        return success
     }
 
-    private fun reloadPdcOfBlock(bukkitBlock: Block) {
-        val pdc = CustomBlockData(bukkitBlock, plugin)
-        if (doesBlockHavePdc(bukkitBlock)) {
-            pdc.remove(plugin.BUILDER_BLOCK_KEY)
-            println("removed pdc")
+    private fun handleSetBlock(wePlayer: Player, bukkitBlock: Block) {
+        if (!plugin.builders.contains(wePlayer.uniqueId)) {
+            // This is for non-builders.
+            if (bukkitBlock.type == Material.AIR) {
+                println("block material is air!")
+                // we want to make sure builder blocks STAY.
+                if (getBlockPdc(bukkitBlock) == null) return
+
+                println("block has pdc!")
+                val pdc = CustomBlockData(bukkitBlock, plugin)
+                val material = Material.getMaterial(pdc.get(plugin.BUILDER_BLOCK_KEY, PersistentDataType.STRING) ?: "") ?: Material.AIR
+                bukkitBlock.type = material // set it back to the way it was before.
+                println("material to set is $material")
+            } else {
+                println("block material is not air!")
+                removePdcOfBlock(bukkitBlock)
+            }
+
+            return // we don't want to do anything else.
         }
+
+        // This is for builders.
+        if (bukkitBlock.type == Material.AIR) {
+            //remove pdc.
+            removePdcOfBlock(bukkitBlock)
+        } else {
+            // if the block type is not air, then set the pdc of that block.
+            setPdcOfBlock(bukkitBlock)
+        }
+    }
+
+    private fun removePdcOfBlock(bukkitBlock: Block) {
+        val pdc = CustomBlockData(bukkitBlock, plugin)
+        if (getBlockPdc(bukkitBlock) == null) return // make sure they actually have the pdc.
+
+        pdc.remove(plugin.BUILDER_BLOCK_KEY)
+        println("removed pdc")
     }
 
     private fun setPdcOfBlock(bukkitBlock: Block) {
-        if (doesBlockHavePdc(bukkitBlock)) return
+        if (getBlockPdc(bukkitBlock) != null) return
         val pdc = CustomBlockData(bukkitBlock, plugin)
         pdc.set(
             plugin.BUILDER_BLOCK_KEY,
@@ -87,8 +96,8 @@ class BlockPlaceExtent(
         println("set pdc")
     }
 
-    private fun doesBlockHavePdc(bukkitBlock: Block): Boolean {
+    private fun getBlockPdc(bukkitBlock: Block): CustomBlockData? {
         val pdc = CustomBlockData(bukkitBlock, plugin)
-        return pdc.has(plugin.BUILDER_BLOCK_KEY)
+        return if (pdc.has(plugin.BUILDER_BLOCK_KEY)) pdc else null
     }
 }
