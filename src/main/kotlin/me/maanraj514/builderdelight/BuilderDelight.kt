7c0b7d992ea78b34d015bf32fba072a3b29a4e4f
YesNoBruhbruh <manrajmaan14@yahoo.com>
2024-07-25 16:26:02 +0800

    First test build for reverting of plugin

--- src/main/kotlin/me/maanraj514/builderdelight/BuilderDelight.kt
+++ src/main/kotlin/me/maanraj514/builderdelight/BuilderDelight.kt
@@ -1,13 +1,8 @@
 package me.maanraj514.builderdelight
 
 import me.maanraj514.builderdelight.command.BuildModeCommand
-import me.maanraj514.builderdelight.command.ConfigReloadCommand
-import me.maanraj514.builderdelight.command.PosCommand
-import me.maanraj514.builderdelight.command.TestClearCommand
 import me.maanraj514.builderdelight.listener.BuildModeListener
-import me.maanraj514.builderdelight.task.ClearBlocksTask
 import me.maanraj514.builderdelight.util.BlocksFile
-import me.maanraj514.builderdelight.util.License
 import me.maanraj514.builderdelight.worldedit.WorldEditListener
 import org.bukkit.Bukkit
 import org.bukkit.Location
@@ -23,15 +18,15 @@ class BuilderDelight : JavaPlugin() {
 //    val BUILDER_BLOCK_KEY = NamespacedKey(this, "builderModeBlock")
 
     lateinit var blocksFile: BlocksFile
-    private lateinit var clearBlocksTask: ClearBlocksTask
 
     override fun onEnable() {
         saveDefaultConfig()
 
-        val licenseKey = config.getString("license-key") ?: "No License"
-        if (!License.isLicenseValid(licenseKey, this)) {
-            Bukkit.getPluginManager().disablePlugin(this)
-        }
+        //TODO this is disabled as this is a dev build of the plugin.
+//        val licenseKey = config.getString("license-key") ?: "No License"
+//        if (!License.isLicenseValid(licenseKey, this)) {
+//            Bukkit.getPluginManager().disablePlugin(this)
+//        }
 
         registerCommands()
         registerListeners()
@@ -41,23 +36,13 @@ class BuilderDelight : JavaPlugin() {
         blocksFile = BlocksFile(this)
         builderBlocks.addAll(blocksFile.loadBlocks()) // add all blocks from file to builderBlocks list.
 
-        clearBlocksTask = ClearBlocksTask(this)
-        clearBlocksTask.runTaskTimer(this, config.getLong("delay"), config.getLong("interval"))
-
-        server.consoleSender.sendMessage("Found WorldEdit! loading support...")
+        server.consoleSender.sendMessage("Found FastAsyncWorldEdit! loading support...")
     }
 
     override fun onDisable() {
         blocksFile.saveBlocks(builderBlocks)
         blocksFile.save()
         println("builderBlocks size is ${builderBlocks.size}")
-
-        for (id in clearBlocksTask.tasks) {
-            Bukkit.getScheduler().cancelTask(id)
-        }
-        clearBlocksTask.tasks.clear()
-
-        clearBlocksTask.cancel()
 
         builders.clear()
         builderBlocks.clear()
@@ -69,14 +54,6 @@ class BuilderDelight : JavaPlugin() {
 
     private fun registerCommands() {
         getCommand("buildmode")?.setExecutor(BuildModeCommand(this))
-
-        val posCommand = PosCommand(this)
-        getCommand("pos1")?.setExecutor(posCommand)
-        getCommand("pos2")?.setExecutor(posCommand)
-        getCommand("savePos")?.setExecutor(posCommand)
-
-        getCommand("builderdelight-reloadconfig")?.setExecutor(ConfigReloadCommand(this))
-        getCommand("testclear")?.setExecutor(TestClearCommand(this))
     }
 
     fun removeBlock(block: Block) {
